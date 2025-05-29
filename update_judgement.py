e677612c6908d5db208bb4d5a0db03ff74a77d61
Ting-Yu Hsu <rye10140210@gmail.com>
2025-05-29 02:00:57 +0100

    Update update_judgement.py

--- update_judgement.py
+++ update_judgement.py
@@ -62,7 +62,7 @@ def set_random_judgement(pages):
     print(f"✅ 今日佳句：{name}")
 
 def send_telegram_message(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
